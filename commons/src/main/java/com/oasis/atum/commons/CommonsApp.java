f369adc4994783ac55347e24b58a6edee3461d4d
BornToRain <1159930219@qq.com>
2017-08-01 18:46:35 +0800

    安全更新

--- commons/src/main/java/com/oasis/atum/commons/CommonsApp.java
+++ commons/src/main/java/com/oasis/atum/commons/CommonsApp.java
@@ -3,7 +3,6 @@ package com.oasis.atum.commons;
 import com.oasis.atum.base.infrastructure.config.AxonConfiguration;
 import com.oasis.atum.base.infrastructure.config.HttpConfiguration;
 import com.oasis.atum.base.infrastructure.config.RedisConfiguration;
-import com.oasis.atum.commons.infrastructure.config.SmsConfiguration;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
@@ -18,21 +17,14 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 @ImportAutoConfiguration(value = {AxonConfiguration.class, RedisConfiguration.class, HttpConfiguration.class})
 public class CommonsApp implements CommandLineRunner
 {
-	private final SmsConfiguration config;
-
-	public CommonsApp(final SmsConfiguration config)
-	{
-		this.config = config;
-	}
 
 	public static void main(final String[] args)
 	{
-		SpringApplication.run(CommonsApp.class,args);
+		SpringApplication.run(CommonsApp.class, args);
 	}
 
 	@Override
 	public void run(final String... args) throws Exception
 	{
-		System.out.println(config.getAccessSecret());
 	}
 }
