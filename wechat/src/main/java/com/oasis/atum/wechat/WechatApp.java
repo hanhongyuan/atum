d4f4b216dc1394c4aa744d5a53e2f72b22b38c30
BornToRain <1159930219@qq.com>
2017-08-02 09:58:36 +0800

    安全更新
    Signed-off-by: BornToRain <1159930219@qq.com>

--- wechat/src/main/java/com/oasis/atum/wechat/WechatApp.java
+++ wechat/src/main/java/com/oasis/atum/wechat/WechatApp.java
@@ -3,6 +3,7 @@ package com.oasis.atum.wechat;
 import com.oasis.atum.base.infrastructure.config.AxonConfiguration;
 import com.oasis.atum.base.infrastructure.config.HttpConfiguration;
 import com.oasis.atum.base.infrastructure.config.RedisConfiguration;
+import com.oasis.atum.base.infrastructure.service.RedisClient;
 import com.oasis.atum.wechat.infrastructure.config.WechatConfiguration;
 import com.oasis.atum.wechat.infrastructure.service.WechatClient;
 import org.springframework.boot.CommandLineRunner;
@@ -24,10 +25,12 @@ import java.time.Duration;
 public class WechatApp implements CommandLineRunner
 {
 	private final WechatClient client;
+	private final RedisClient redis;
 
-	public WechatApp(final WechatClient client)
+	public WechatApp(final WechatClient client, final RedisClient redis)
 	{
 		this.client = client;
+		this.redis = redis;
 	}
 
 	public static void main(final String[] args)
@@ -38,9 +41,13 @@ public class WechatApp implements CommandLineRunner
 	@Override
 	public void run(final String... args) throws Exception
 	{
+		System.out.println(redis);
+
 		//每隔110分钟运行一次
 		Flux.interval(Duration.ofMinutes(110L), Schedulers.parallel())
 			.subscribe(l -> client.initWechat());
+
+		redis.get("AccessToken").subscribe(System.out::println);
 
 	}
 }
